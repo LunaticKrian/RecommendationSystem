a338e9ff17d0729db52f8457a6dd8c52bb86e76e
ascend <1341942899@qq.com>
2022-10-20 17:09:47 +0800

    新增企业状态、税率枚举类

--- SystemParent/system-core/src/main/java/com/mislab/core/systemcore/common/enums/EnterpriseStateEnum.java
+++ SystemParent/system-core/src/main/java/com/mislab/core/systemcore/common/enums/EnterpriseStateEnum.java
@@ -13,11 +13,12 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 public enum EnterpriseStateEnum {
+
     //企业信息已不存在
     ALREADY_DELETE(0),
-    //企业信息正在填写中
+    //企业信息正在填写中，未完成
     STILL_WRITING(1),
-    //企业信息已提交
+    //企业信息已提交，已完成
     ALREADY_COMPLETE(2);
 
     private final Integer code;
