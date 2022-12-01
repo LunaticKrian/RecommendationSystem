051d1069ea3a887fbeb6d9f9a9f09f8c22df4c1e
Krian <2793260947@qq.com>
2022-12-01 13:22:05 +0800

    修改，删除冗余代码！ —— 2022/12/01

--- SystemParent/system-core/src/main/java/com/mislab/core/systemcore/service/impl/EnterpriseBusinessServiceImpl.java
+++ SystemParent/system-core/src/main/java/com/mislab/core/systemcore/service/impl/EnterpriseBusinessServiceImpl.java
@@ -1,17 +1,10 @@
 package com.mislab.core.systemcore.service.impl;
 
-import com.mislab.common.result.R;
-import com.mislab.core.systemcore.mapper.EnterpriseMapper;
-import com.mislab.core.systemcore.pojo.dto.EnterpriseBusinessDTO;
-import com.mislab.core.systemcore.pojo.entity.EnterpriseBusiness;
-import com.mislab.core.systemcore.mapper.EnterpriseBusinessMapper;
-import com.mislab.core.systemcore.pojo.dto.Enterprise2VO;
-import com.mislab.core.systemcore.service.EnterpriseBusinessService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mislab.core.systemcore.mapper.EnterpriseBusinessMapper;
+import com.mislab.core.systemcore.pojo.entity.EnterpriseBusiness;
+import com.mislab.core.systemcore.service.EnterpriseBusinessService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * <p>
@@ -23,42 +16,5 @@ import java.util.List;
  */
 @Service
 public class EnterpriseBusinessServiceImpl extends ServiceImpl<EnterpriseBusinessMapper, EnterpriseBusiness> implements EnterpriseBusinessService {
-
-    @Autowired
-    EnterpriseBusinessMapper enterpriseBusinessMapper;
-
-    @Autowired
-    EnterpriseMapper enterpriseMapper;
-    /**
-     * 在第一个页面已将建好enterpriseBusiness，只需修改
-     * @param EPBs
-     * @return
-     */
-    @Override
-    public R updateByEnterpriseKey(List<EnterpriseBusinessDTO> EPBs,String enterpriseKey) {
-        for (EnterpriseBusinessDTO enterpriseBusiness: EPBs) {
-            enterpriseBusiness.setEnterpriseKey(enterpriseKey);
-            enterpriseBusinessMapper.updateByEnterpriseKey(enterpriseBusiness);
-        }
-        return R.SUCCESS();
-    }
-
-    @Override
-    public List<EnterpriseBusiness> getByEnterpriseKey(String enterpriseKey) {
-        List<EnterpriseBusiness> EPBs = enterpriseBusinessMapper.getByEnterpriseKey(enterpriseKey);
-        return EPBs;
-    }
-
-    @Override
-    public int setSTATAC(String enterpriseKey,int ST,double AT, double AC) {
-        enterpriseMapper.updateSAAEnterprise(enterpriseKey,ST,AT,AC);
-        return 1;
-    }
-
-    @Override
-    public Enterprise2VO getDateIITable(String enterpriseKey) {
-        Enterprise2VO enterprise2VO = enterpriseMapper.selectDateIITable(enterpriseKey);
-        return enterprise2VO;
-    }
 
 }
